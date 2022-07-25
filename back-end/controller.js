c079f8a6d5b716b1b24e80e1fb34984267cf422b
Carlo Arayon <cearayon@gmail.com>
2022-07-25 16:15:57 -0700

    first push, created files, set up server and controller

---
+++ back-end/controller.js
@@ -0,0 +1,57 @@
+//here we import the db info from our json file to use the information in there
+const games = require('./db.json')
+let globalId = 4
+
+//export the controller functions for use in our server file
+
+module.exports = {
+    sendGames: (req,res) => {
+        res.status(200).send(games)
+    },
+
+    createGame: (req,res) => {
+        const {name, rating, imageURL, genre, comment} = req.body
+
+        let newGame= {
+            name,
+            rating,
+            imageURL,
+            genre,
+            comment,
+            id: globalId
+        }
+        games.push(newGame)
+        res.status(200).send(games)
+        globalId++
+    },
+
+    updateGame: (req, res) => {
+        const existingId = +req.params.id
+        let index = games.findIndex(game => game.id === existingId)
+        
+        if(req.body.type === 'plus'){
+            if(games[index].rating >= 5){
+                res.status(400).send('Cannot rate movie beyond 5.');
+            } else {
+                games[index].rating++;
+                res.status(200).send(games)
+            }
+        } else {
+            if(games[index].rating <= 1){
+                res.status(400).send('Cannot rate movie below 1.');
+            } else {
+                movies[index].rating--;
+                res.status(200).send(games)
+            }
+        }
+    },
+
+    deleteGame: (req, res) => {
+        const existingId = +req.params.id
+        
+        let index = games.findIndex(game => game.id === existingId);
+        games.splice(index, 1);
+        res.status(200).send(games);
+    }
+
+}
---
+++ back-end/server.js
@@ -0,0 +1,26 @@
+//boilerplate for server setup//
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+//end boilerplate code for server//
+
+//import controller functions//
+const {sendGames, createGame, updateGame, deleteGame} = require('./controller.js')
+//import complete
+
+//endpoints for our requests//
+app.get('/api/games', sendGames);
+app.post('/api/games', createGame);
+app.put('/api/games/:id', updateGame);
+app.delete('/api/movies/:id', deleteGame);
+//endpoints complete//
+
+//port stuff boilerplate//
+const port = process.env.PORT || 4004
+app.listen(port, () => {
+    console.log(`in at port ${port} boyooooo`)
+})
+//port boilerplate complete//
